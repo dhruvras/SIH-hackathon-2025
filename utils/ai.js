b5d60bde4c45e74f319b8a0e1ae61045dc890177
dhrurast <dhrurast@gmail.com>
2025-11-16 03:20:33 +0530

    starting annknown2.0

--- utils/ai.js
+++ utils/ai.js
@@ -1,6 +1,6 @@
 export async function sendPrompt(prompt) {
   try {
-    const response = await fetch("http://192.168.180.132:8000/generate", {
+    const response = await fetch("http://192.168.5.72:8000/generate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
